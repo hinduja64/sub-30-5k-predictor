fdf96c08f6c34e1abaacb85eb4cacccf8e83bfe3
Hinduja Reddy <134975602+hinduja64@users.noreply.github.com>
2026-04-30 11:52:30 +0530

    Update merge_data.py

--- scripts/merge_data.py
+++ scripts/merge_data.py
@@ -1,1 +1,5 @@
-
+import pandas as pd
+fitbit = pd.read_csv('fitbit_health_metrics.csv')
+running = pd.read_csv('running_logs.csv')
+master = pd.merge(running, fitbit, on='date', how='outer')
+master.to_csv('master_running_data.csv', index=False)
